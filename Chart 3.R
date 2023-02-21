648149b5c9450d02a3d3fe0ecc9e114fa68f706a
Flora704 <ghlee704@uw.edu>
2023-02-20 16:11:23 -0800

    flora chart 3

--- Chart 3.R
+++ Chart 3.R
@@ -4,10 +4,11 @@ library("ggplot2")
 library("scales")
 
 
+Insurance_df <- read.csv("https://raw.githubusercontent.com/stedy/Machine-Learning-with-R-datasets/master/insurance.csv")
 
 num_children <- Insurance_df %>% group_by(children) %>% summarize(num = n())
 
-pie_chart <- ggplot(data = num_children, aes(x="", y= num, fill=factor(children))) + 
+ggplot(data = num_children, aes(x="", y= num, fill=factor(children))) + 
   geom_bar(width = 1, stat = "identity") +
   coord_polar("y", start=0) +
   ggtitle("Distribution of demographics with the number of children") + 
